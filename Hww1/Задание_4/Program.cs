e84a147b9eff14a148abc9102aba236ebee5abbc
Timurgess <timurgess@yandex.ru>
2022-06-28 10:54:25 +0300

    Четвертое Задание

---
+++ Hww1/Задание_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while(number < N)
+{
+ if (number % 2 == 0)
+ {
+      Console.Write(number + "");
+ }
+    number = number + 1;
+}
